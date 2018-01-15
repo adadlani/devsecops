525caeaf2551c5eff60af93a12d0c52c86481e42
adadlani <anel.dadlani@gmail.com>
2018-01-15 07:39:59 -0500

    Create centos_initial_install.sh

---
+++ scripts/centos_initial_install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Script to execute on a fresh install of CentOS 7.x to:
+#  Perform system update
+#  Enable rhui-REGION-rhel-server-optional repository (e.g. awscli)
+#  Add EPEL repository (e.g collectd)
+#  Install packages:
+#   GIT
+#   AWSCLI
+#  Configure GIT
+#  Clone repo
+# Typically user gets the file using curl:
+# curl -O https://raw.githubusercontent.com/adadlani/devsecops/master/scripts/rhel_initial_install.sh \
+# --silent
+# Assumptions:
+#  - Executing as ec2-user which can execute sudo
+#  - EC2 member of IAM role so we can execute AWS CLI
+# Required arguments:
+#  GH_USER username
+#  GH_REPO repo (e.g. myrepo.git)
+#  GH_CFG_NAME fullname (e.g. John Doe)
+#  GH_CFG_EMAIL email (e.g. JohnDoe@JD.COM)
+
+# Display help if 4 arguments not passed
+if [[ $# -ne 4 ]] ; then
+  echo Usage:
+  echo    script GH_USER GH_REPO GH_CFG_NAME GH_CFG_EMAIL
+  exit 1
+fi
+
+# Update system
+sudo yum update -y
+
+# Enable rhui-REGION-rhel-server-optional repo
+# /etc/yum.repos.d/redhat-rhui.repo
+sudo yum-config-manager --enable rhui-REGION-rhel-server-optional
+
+# Install EPEL
+curl -O https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm --silent
+sudo yum install epel-release-latest-7.noarch.rpm -y
+
+# Install GIT
+echo Installing GIT...
+sudo yum install git -y
+
+# Install AWSCLI (yum or pip?  AWS prefers pip)
+echo Installing AWSCLI...
+#sudo yum install awscli
+curl -O https://bootstrap.pypa.io/get-pip.py --silent
+python get-pip.py --user  # Installs pip in ~/.local/bin which is by default in $PATH
+pip install awscli --upgrade --user
+
+# Configure GIT (HTTPS protocol assumed)
+echo Configuring GIT...
+git config --global push.default simple
+git config --global credential.helper 'cache --timeout 7200'
+git config --global user.name "$3"
+git config --global user.email $4
+
+echo Cloning repo...
+git clone https://github.com/$1/$2
+
+# Following requires an existing repo (e.g. .git/config)
+echo Configuring GITHub credentials....
+cd ${2%.*}  # Get repo base name from $2
+git config credentials.helper store
+git push https://github.com/$1/$2 --all
