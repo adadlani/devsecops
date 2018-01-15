945f310b846ea7f0d9720a756bac99f208a038f7
Anel Dadlani <Anel.Dadlani@gmail.com>
2018-01-15 00:43:25 +0000

    Pseudo logic for snapshot scripts

--- scripts/create_snapshot.sh
+++ scripts/create_snapshot.sh
@@ -7,3 +7,16 @@
 #
 # Requirements:
 #  If running in the instance then it must be part of an IAM role member
+
+INSTANCE_ID=$1
+
+# Get following of instance: 
+#  Root device (e.g. /dev/sda1)
+#  Block devices (e.g. /dev/sda1)
+#  EBS ID (e.g. vol-1234)
+
+# Create snapshot of EBS ID
+# Assign:  Name and Descriptiona
+# Returns: SnapShotID
+
+# Wait till snapshot completes (takes a few minutes)
