93182d581ee90cfc413527c0742c7744da2a222c
adadlani <anel.dadlani@gmail.com>
2018-01-20 08:18:29 -0500

    Update install_mssql.sh

--- scripts/mssql/install_mssql.sh
+++ scripts/mssql/install_mssql.sh
@@ -4,14 +4,27 @@
 # Update system
 sudo yum update -y
 
-# Add repository
+# Add repositories
+# mssql-server
 sudo curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/7/mssql-server-2017.repo
 
-# Install SQL (Database engine)
+# List all mssql packages:
+sudo yum search mssql
+# mssql-server.x86_64 : Microsoft SQL Server Relational Database Engine
+# mssql-server-agent.x86_64 : Microsoft SQL Server Agent
+# mssql-server-fts.x86_64 : Microsoft SQL Server Full Text Search
+# mssql-server-ha.x86_64 : High Availability support for Microsoft SQL Server
+#                       : Relational Database Engine
+# mssql-server-is.x86_64 : Microsoft SQL Server Integration Services
+
+# Client tools
+sudo curl -o /etc/yum.repos.d/ms-prod.repo https://packages.microsoft.com/config/rhel/7/prod.repo
+
+# Install SQL (Database engine), agent and client tools
 # Default destinations:
 #  /opt/mssql (Progam binaries & libraries)
 #  /var/opt/mssql (Databases and log files, log directory and mssql.conf)
-sudo yum install mssql-server -y
+sudo yum install mssql-server mssql-server-agent mssql-tools -y
 
 # Initial setup
 # sudo /opt/msql/bin/mssql-conf setup
