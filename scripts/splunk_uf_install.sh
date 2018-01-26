9250166fcfe8d130ff71d1910fd4e57c418c2933
adadlani <anel.dadlani@gmail.com>
2018-01-25 21:13:45 -0500

    Create splunk_uf_install.sh

---
+++ scripts/splunk_uf_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -a
+# Script to insall Splunk Universal Forwarder on a CentOS 7x 
+# Logged in as user centos which is on sudo-ers list
+# Assumes EC2 instance is an appropriate IAM role to access S3 bucket
+# Required arguments:
+#  BUCKET Contains the Splunk UF installation RPM
+
+# Display help if required argument(s) not passed
+if [[ $# -ne 1 ]] ; then
+  echo Usage:
+  echo    script BUCKET
+  exit 1
+fi
+
+BUCKET=$1
+LATEST_SPLUNK_UF_RPM=splunkforwarder-7.0.1-2b5b15c4ee89-linux-2.6-x86_64.rpm
+
+# Update system
+sudo yum update -y
+
+# Download latest Slunk
+aws s3 cp s3://$BUCKET/$LATEST_SPLUNK_UF_RPM $HOME/download
+
+# Install Splunk UF
+# Note error: useradd: cannot create directory /opt/splunk
+# but installation seems to continue OK and /opt/splunk is created
+# and newly created user splunk has owner-ship
+sudo rpm -ivh $HOME/download/$LATEST_SPLUNK_UF_RPM
+
+# Configure
+
+# Start
