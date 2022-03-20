9b428e7801084259d8d673695ccf543512face0d
EC2 Default User <ec2-user@ip-172-31-52-212.ec2.internal>
2022-03-20 23:00:18 +0000

    Initial check-in for Splunk SDK

--- scripts/splunk-sdk-test.py
+++ scripts/splunk-sdk-test.py
@@ -1,1 +1,26 @@
+import splunklib.client as client
+from dotenv import load_dotenv
+import os
 
+
+def main():
+    try:
+        load_dotenv()
+        username = os.environ.get('username')
+        password = os.environ.get('password')
+        host = os.environ.get('splunk-host')
+        port = '8089'
+        owner = os.environ.get('splunk-owner')
+        app = 'search'
+        sharing = 'user'
+        service = client.connect(username=username, password=password,
+                                 host=host, port=port, owner=owner, app=app,
+                                 sharing=sharing)
+        if service:
+            print("Splunk connection successfull")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
